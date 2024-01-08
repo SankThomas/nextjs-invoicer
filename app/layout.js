92fb4999d9b3886e1715836720d27e4d547215de
sankthomas <thomassankara74@gmail.com>
2024-01-08 19:36:34 +0300

    update metadata

--- app/layout.js
+++ app/layout.js
@@ -2,14 +2,21 @@ import { Roboto } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 
-const roboto = Roboto({ subsets: ["latin"], weight: ["400", "700" ] });
+const roboto = Roboto({ subsets: ["latin"], weight: ["400", "700"] });
 
 export const metadata = {
+  metadataBase: new URL("https://invoicerv2.vercel.app"),
   title: {
     default: "Invoicer | Generate and Download Invoices",
     template: "%s | Invoicer | Generate and Download Invoices",
   },
   description: "Easily generate invoices for yourself and your clients.",
+  twitter: {
+    card: "summary_large_image",
+  },
+  openGraph: {
+    images: "./opengraph-image.png",
+  },
 };
 
 export default function RootLayout({ children }) {
