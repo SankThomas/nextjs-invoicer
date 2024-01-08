7cc1b76784256c9912fa28678bb067b7d28a518a
sankthomas <thomassankara74@gmail.com>
2024-01-08 22:58:46 +0300

    chore: add table to display items, add footer on invoice preview

--- app/dashboard/_components/invoiceview.jsx
+++ app/dashboard/_components/invoiceview.jsx
@@ -4,12 +4,16 @@ import { format } from "date-fns";
 export default function InvoiceView({ values }) {
   return (
     <>
-      <section className="bg-white p-4 rounded border space-y-8 max-h-full overflow-auto">
+      <section className="bg-white p-4 rounded border space-y-8">
         <div className="text-right">
           <h2 className="text-lg lg:text-xl font-bold">{values.name}</h2>
           <p>{values.address}</p>
-          <p>Bank Name: {values.bankName}</p>
-          <p>Bank Account number: {values.accountNumber}</p>
+          <p>
+            <strong>Bank Name:</strong> {values.bankName}
+          </p>
+          <p>
+            <strong>Bank Account number:</strong> {values.accountNumber}
+          </p>
         </div>
 
         <div>
@@ -32,37 +36,67 @@ export default function InvoiceView({ values }) {
         </div>
 
         <div>
-          <ul className="bg-slate-200 p-1 rounded flex items-center justify-between text-sm font-bold text-slate-900">
-            <li>Item</li>
-            <li>Quantity</li>
-            <li>Price</li>
-            <li>Total</li>
-          </ul>
-
-          <ul className="p-1 rounded flex items-center justify-between text-sm text-slate-900">
+          <table width="100%" className="mb-10">
+            <thead>
+              <tr className="bg-gray-100 p-1">
+                <td className="font-bold">Item Name</td>
+                <td className="font-bold">Quantity</td>
+                <td className="font-bold">Price</td>
+                <td className="font-bold">Total</td>
+              </tr>
+            </thead>
             {values.items.map((item) => (
-              <>
-                <li>{item.item}</li>
-                <li>{item.quantity}</li>
-                <li>{item.price}</li>
-                <li>{item.total}</li>
-              </>
+              <React.Fragment key={item.id}>
+                <tbody>
+                  <tr className="h-10">
+                    <td>{item.item}</td>
+                    <td>{item.quantity}</td>
+                    <td>{item.price.toLocaleString()}</td>
+                    <td>{item.total.toLocaleString()}</td>
+                  </tr>
+                </tbody>
+              </React.Fragment>
             ))}
-          </ul>
-
-          <div className="grid gap-2 mt-2">
-            <ul className="flex items-center justify-between"></ul>
-          </div>
+          </table>
         </div>
 
         <div>
-          <h2 className="text-3xl font-bold">Kshs. {values.totalAmount}</h2>
+          <h2 className="text-3xl font-bold">
+            Kshs. {values.totalAmount.toLocaleString()}
+          </h2>
         </div>
 
         <div>
-          <p>Additional notes to the client</p>
+          <p className="text-slate-900 mb-2">
+            <strong>Additional notes to the client</strong>
+          </p>
 
           <p>{values.notes}</p>
+        </div>
+
+        <div className="border-t pt-8">
+          <ul className="flex flex-wrap items-center justify-center gap-4 text-xs">
+            <li>
+              <strong>Name: </strong>
+              {values.name}
+            </li>
+            <li>
+              <strong>Email: </strong>
+              {values.email}
+            </li>
+            <li>
+              <strong>Bank Account Holder: </strong>
+              {values.name}
+            </li>
+            <li>
+              <strong>Bank Account Number: </strong>
+              {values.accountNumber}
+            </li>
+            <li>
+              <strong>Phone: </strong>
+              {values.phoneNumber}
+            </li>
+          </ul>
         </div>
       </section>
     </>
